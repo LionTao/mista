9cc2cd133a7c1414a806f80b7b7e7603a0e6ac33
LionTao <taojiachun980831@163.com>
2022-05-10 00:35:17 +0800

    finish pns

--- node/packages/pns-daemon/src/index.ts
+++ node/packages/pns-daemon/src/index.ts
@@ -1,8 +1,9 @@
-import {DaprClient, DaprServer} from "@dapr/dapr/src/index";
+import { DaprServer, HttpMethod } from "@dapr/dapr/src/index";
 import RBush from "rbush";
-import {PNSEntry} from "../../types/PNSEntry";
-import {getRes0Indexes, h3SetToMultiPolygon} from "h3-js";
+import { PNSEntry } from "../../types/PNSEntry";
+import { getRes0Indexes, h3SetToMultiPolygon } from "h3-js";
 import * as proj4 from "proj4";
+import RWLock from "async-rwlock";
 
 const daprHost = "127.0.0.1";
 const daprPort = "50000"; // Dapr Sidecar Port of this Example Server
@@ -10,29 +11,41 @@ const serverHost = "127.0.0.1"; // App Host of this Example Server
 const serverPort = "50001"; // App Port of this Example Server
 
 let rtree = new RBush<PNSEntry>();
-
+const lock = new RWLock();
 interface UpdateData {
     mother: string;
     children: Array<string>;
 }
 
-async function update(data: UpdateData) {
+async function update(data: UpdateData): Promise<void> {
+    await lock.writeLock();
+    rtree.remove(h3ToBBox(data.mother), (a, b) => a.id === b.id);
+    rtree.load(data.children.map(h3ToBBox));
+    lock.unlock;
+    return;
+}
 
+async function query(): Promise<string> {
+    await lock.readLock();
+    const res = JSON.stringify(rtree.toJSON());
+    lock.unlock;
+    return res;
 }
 
 async function start() {
     // Create a Server (will subscribe) and Client (will publish)
     const server = new DaprServer(serverHost, serverPort, daprHost, daprPort);
-    const client = new DaprClient(daprHost, daprPort);
+    await lock.writeLock();
+    rtree.load(getRes0Indexes().map(h3ToBBox));
+    lock.unlock();
 
-    for (let h in getRes0Indexes) {
+    await server.invoker.listen("update", (data) => update(JSON.parse(data.body)), { method: HttpMethod.POST });
+    await server.invoker.listen("query", () => query(), { method: HttpMethod.GET });
 
-    }
-
-    await server.invoker.listen("update")
+    await server.start();
 }
 
-function h3ToBBox(h: string) {
+function h3ToBBox(h: string): PNSEntry {
     const coordinates = h3SetToMultiPolygon([h], false)[0][0];
     const fromProjection = proj4.Proj('EPSG:4326');
     const toProjection = proj4.Proj("EPSG:3857");
@@ -54,6 +67,7 @@ function h3ToBBox(h: string) {
         maxY: maxY,
         id: h
     }
+    return res;
 }
 
 start().catch((e) => {
